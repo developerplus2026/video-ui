efa65bc907d14204852dd40361f077b407663040
developerplus2026 <anpham08112009@gmail.com>
2026-06-18 10:50:11 +0700

    fix: restore border and background styling in Page component for layout consistency

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center border border-input rounded-xl bg-[#0c0c0c] h-screen w-screen p-6">
-      <div className="w-[calc(100vw-200px)] h-[calc(100vh-80px)]  ">
+    <div className="flex justify-center items-center  h-screen w-screen p-6">
+      <div className="w-[calc(100vw-200px)] border border-input rounded-xl bg-[#0c0c0c] h-[calc(100vh-80px)]  ">
 
       </div>
     </div>
