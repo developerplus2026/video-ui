f9494f0c3af0b095bb36ef6ba8e04b3e3b7d7075
developerplus2026 <anpham08112009@gmail.com>
2026-06-18 16:21:17 +0700

    feat: implement sidebar navigation with team and project management components

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,21 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
 import { Input } from "@/components/ui/input"
 import { EmptyUI } from "@/components/ui-system/empty-ui"
 export default function Page() {
@@ -124,7 +139,36 @@ export default function Page() {
           </div>
         </div>
         <div className="flex h-full w-full items-center justify-center">
-          <EmptyUI />
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <header className="flex h-16 shrink-0 items-center gap-2">
+                <div className="flex items-center gap-2 px-4">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                  />
+                  <Breadcrumb>
+                    <BreadcrumbList>
+                      <BreadcrumbItem className="hidden md:block">
+                        <BreadcrumbLink href="#">
+                          Build Your Application
+                        </BreadcrumbLink>
+                      </BreadcrumbItem>
+                      <BreadcrumbSeparator className="hidden md:block" />
+                      <BreadcrumbItem>
+                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                      </BreadcrumbItem>
+                    </BreadcrumbList>
+                  </Breadcrumb>
+                </div>
+              </header>
+              <div>
+                <EmptyUI />
+              </div>
+            </SidebarInset>
+          </SidebarProvider>
         </div>
       </div>
     </div>
