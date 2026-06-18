9b819024362109bc60db664aace5efe52ec380d0
developerplus2026 <anpham08112009@gmail.com>
2026-06-18 10:48:38 +0700

    fix: correct dimensions in Page component for proper layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Page() {
   return (
     <div className="flex justify-center items-center border border-input rounded-xl bg-[#0c0c0c] h-screen w-screen p-6">
-      <div className="w-[calc(100vh-200px)] h-[calc(100vh-40px)]  ">
+      <div className="w-[calc(100vw-200px)] h-[calc(100vh-80px)]  ">
 
       </div>
     </div>
