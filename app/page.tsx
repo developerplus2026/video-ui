01d2afe58f8fede6ab2454627d1f732951355fca
developerplus2026 <anpham08112009@gmail.com>
2026-06-18 16:05:05 +0700

    fix: refactor dropdown menu and input group for improved structure and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
 import { Button } from "@/components/ui/button"
-import { Setting06FreeIcons, Setting06Icon, Setting07Icon, SettingDone01FreeIcons, SettingDone04FreeIcons, User02FreeIcons } from "@hugeicons/core-free-icons"
+import {
+  Search,
+  Setting06FreeIcons,
+  Setting06Icon,
+  Setting07Icon,
+  SettingDone01FreeIcons,
+  SettingDone04FreeIcons,
+  User02FreeIcons,
+} from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,74 +28,99 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
 export default function Page() {
   return (
-    <div className="flex justify-center items-center  h-screen w-screen p-6">
-      <div className="w-[calc(100vw-200px)] border border-input rounded-xl bg-[#0c0c0c] h-[calc(100vh-80px)]  ">
-        <div className="h-15 w-full border-b flex justify-between items-center p-6 border-input ">
-          <div className="flex flex-col ">
-            <p className="text-sm">A modern video window is under development.</p>
-            <p className="text-xs text-[#a1a1a1]">It will be released soon on July 6, 2026.</p>
+    <div className="flex h-screen w-screen items-center justify-center p-6">
+      <div className="h-[calc(100vh-80px)] w-[calc(100vw-200px)] rounded-xl border border-input bg-[#0c0c0c]">
+        <div className="flex h-15 w-full items-center justify-between border-b border-input p-6">
+          <div className="flex flex-col">
+            <p className="text-md">
+              A modern video window is under development.
+            </p>
+            <p className="text-sm text-[#a1a1a1]">
+              It will be released soon on July 6, 2026.
+            </p>
+          </div>
+          <div>
+            <InputGroup className="max-w-xs">
+              <InputGroupInput placeholder="Search..." />
+              <InputGroupAddon>
+                <HugeiconsIcon icon={Search} size={"30"} strokeWidth={2} />
+              </InputGroupAddon>
+              <InputGroupAddon align="inline-end">12 results</InputGroupAddon>
+            </InputGroup>
           </div>
           <div className="flex items-center gap-4">
-<Button className="" variant={'outline'}>Getting Started</Button>
-<Button className="" variant={'outline'}>Contact Sale</Button>
-<DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size={'icon-lg'}><HugeiconsIcon icon={User02FreeIcons} size={'30'} strokeWidth={2} /></Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-40" align="end">
-              <DropdownMenuGroup>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuItem>
-                  Profile
-                  <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Billing
-                  <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Settings
-                  <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuSub>
-                  <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-                  <DropdownMenuPortal>
-                    <DropdownMenuSubContent>
-                      <DropdownMenuItem>Email</DropdownMenuItem>
-                      <DropdownMenuItem>Message</DropdownMenuItem>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuItem>More...</DropdownMenuItem>
-                    </DropdownMenuSubContent>
-                  </DropdownMenuPortal>
-                </DropdownMenuSub>
-                <DropdownMenuItem>
-                  New Team
-                  <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>GitHub</DropdownMenuItem>
-                <DropdownMenuItem>Support</DropdownMenuItem>
-                <DropdownMenuItem disabled>API</DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  Log out
-                  <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
+            <Button className="" variant={"outline"}>
+              Getting Started
+            </Button>
+            <Button className="" variant={"outline"}>
+              Contact Sale
+            </Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size={"icon-lg"}>
+                  <HugeiconsIcon
+                    icon={User02FreeIcons}
+                    size={"30"}
+                    strokeWidth={2}
+                  />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-40" align="end">
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuItem>
+                    Profile
+                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Billing
+                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Settings
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>
+                      Invite users
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent>
+                        <DropdownMenuItem>Email</DropdownMenuItem>
+                        <DropdownMenuItem>Message</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>More...</DropdownMenuItem>
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+                  <DropdownMenuItem>
+                    New Team
+                    <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>GitHub</DropdownMenuItem>
+                  <DropdownMenuItem>Support</DropdownMenuItem>
+                  <DropdownMenuItem disabled>API</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    Log out
+                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
-          
         </div>
       </div>
     </div>
